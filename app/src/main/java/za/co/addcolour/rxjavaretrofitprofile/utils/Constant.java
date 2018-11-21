3ae806d40b93788bf1569f635a933c819395d30f
Mpho Nkuna <mcnkuna@addcolour.co.za>
2018-11-21 14:20:05 +0200

    Using Rxjava, Retrofit, viewModel and Repository

--- app/src/main/java/za/co/addcolour/rxjavaretrofitprofile/utils/Constant.java
+++ app/src/main/java/za/co/addcolour/rxjavaretrofitprofile/utils/Constant.java
@@ -3,5 +3,5 @@ package za.co.addcolour.rxjavaretrofitprofile.utils;
 public class Constant {
 
     public static final String BASE_URL = "https://my.api.mockaroo.com/";
-    public static final String API_KEY = "77132ba0";
+    public static final String API_KEY = "YOUR_API_KEY";
 }
